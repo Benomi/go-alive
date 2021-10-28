8ff05350da264032a3068e342df25779cb39e515
Benomi Tekle <benomi@protonmail.com>
2021-10-28 10:53:23 +0300

    provide strategy for notifiers

--- strategies/http-status.go
+++ strategies/http-status.go
@@ -12,37 +12,31 @@ import (
 type HttpStatusStrategy struct{}
 
 func (p HttpStatusStrategy) Run(configuration c.TargetConfigurations) HealthCheckResult {
-	healthCheckResult := HealthCheckResult{NumberOfUnreachableServices: 0, Host: configuration.Ip, Results: []SpecificPortHealthCheckResult{}}
+	healthCheckResult := HealthCheckResult{NumberOfUnreachableServices: 0, Host: configuration.Ip, Strategy: "http-response-status", Results: []SpecificPortHealthCheckResult{}}
 
 	for _, portConfig := range configuration.Ports {
 		portScanResult := SpecificPortHealthCheckResult{
 			Host: configuration.Ip,
 			Port: portConfig.Port, IsReachable: false,
-			Error: nil,
+			Error:    nil,
+			Strategy: "http-response-status",
 		}
 
 		protocol := "https"
-
 		if !configuration.Https {
 			protocol = "http"
 		}
-
 		location := strings.TrimSpace(fmt.Sprintf("%s://%s:%d/health-check", protocol, configuration.Ip, portConfig.Port))
-
 		fmt.Printf("[+] Running http status check on %s\n", location)
 		request, err := http.NewRequest("GET", location, nil)
-
 		if err != nil {
 			fmt.Println("HTTP call failed:", err)
 			log.Println(err)
 			portScanResult.Error = err
 			healthCheckResult.NumberOfUnreachableServices += 1
 			healthCheckResult.Results = append(healthCheckResult.Results, portScanResult)
-
 			continue
-
 		}
-
 		request.Header.Set("Content-Type", "application/json")
 		client := &http.Client{}
 		response, err := client.Do(request)
@@ -52,25 +46,18 @@ func (p HttpStatusStrategy) Run(configuration c.TargetConfigurations) HealthChec
 			portScanResult.Error = err
 			healthCheckResult.NumberOfUnreachableServices += 1
 			healthCheckResult.Results = append(healthCheckResult.Results, portScanResult)
-
 			continue
 		}
-
 		defer response.Body.Close()
-
 		if response.StatusCode != http.StatusOK {
 			log.Println(err)
 			portScanResult.Error = err
 			healthCheckResult.NumberOfUnreachableServices += 1
 			healthCheckResult.Results = append(healthCheckResult.Results, portScanResult)
-
 			continue
-
 		}
-
 		portScanResult.IsReachable = true
 		healthCheckResult.Results = append(healthCheckResult.Results, portScanResult)
-
 	}
 	return healthCheckResult
 }
