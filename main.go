a65c262d080616df65dea2d0f0397888bfc565d3
Benomi Tekle <benomi@protonmail.com>
2021-10-31 01:59:50 +0300

    add missing err checkings

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ type portConfigurationsStrategyCheck struct {
 // HealthCheckStrategyChooser - struct to resolve a scanning strategy by it's name
 type HealthCheckStrategyChooser struct{}
 
+// Parse - Function to dynamically choose scanning algorithm
 func (runner *HealthCheckStrategyChooser) Parse(configuration c.TargetConfigurations) (s.Strategy, error) {
 	var portConfigurationsStrategyCheck portConfigurationsStrategyCheck
 	mapstructure.Decode(configuration, &portConfigurationsStrategyCheck)
@@ -48,12 +49,14 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 		healthCheckResult := strategy.Run(targetConfig)
 		for i, portToScan := range targetConfig.Ports {
 			for _, notificationReceiver := range portToScan.Notify {
+
 				var notificationStrategyConfig c.NotificationStrategyConfig
-				mapstructure.Decode(notificationReceiver, &notificationStrategyConfig)
+				utils.Check(mapstructure.Decode(notificationReceiver, &notificationStrategyConfig))
+
 				switch notificationStrategyConfig.Via {
 				case "telegram":
 					var telegramNotificationConfig c.TelegramNotificationConfig
-					mapstructure.Decode(notificationReceiver, &telegramNotificationConfig)
+					utils.Check(mapstructure.Decode(notificationReceiver, &telegramNotificationConfig))
 					bot, ok := notificationConfigs.Telegram.TelegramBotsMap[telegramNotificationConfig.From]
 					if !ok {
 						fmt.Println("Error: Bot not found in config.", telegramNotificationConfig.From)
@@ -62,13 +65,17 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 					if !ok {
 						fmt.Println("Error: Bot not found in config.", telegramNotificationConfig.Chat)
 					}
+
 					notifier := n.NewTelegramNotifier(bot, chat)
-					notifier.NotifySpecificPortHealthCheckResult(healthCheckResult.Results[i], telegramNotificationConfig.Template)
+					err = notifier.NotifySpecificPortHealthCheckResult(healthCheckResult.Results[i], telegramNotificationConfig.Template)
+					if err != nil {
+						fmt.Println(err)
+					}
 					// fmt.Println("telegram notification", telegramNotificationConfig.)
 
 				case "slack":
 					var slackNotificationConfig c.SlackNotificationConfig
-					mapstructure.Decode(notificationReceiver, &slackNotificationConfig)
+					utils.Check(mapstructure.Decode(notificationReceiver, &slackNotificationConfig))
 					app, ok := notificationConfigs.Slack.SlackAppsMap[slackNotificationConfig.From]
 					if !ok {
 						fmt.Println("Error: Slack App not found in config.", slackNotificationConfig.From)
@@ -79,12 +86,15 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 					}
 
 					notifier := n.NewSlackNotifier(app, channel)
-					notifier.NotifySpecificPortHealthCheckResult(healthCheckResult.Results[i], slackNotificationConfig.Template)
+					err = notifier.NotifySpecificPortHealthCheckResult(healthCheckResult.Results[i], slackNotificationConfig.Template)
+					if err != nil {
+						fmt.Println(err)
+					}
 					// fmt.Println("telegram notification", telegramNotificationConfig.)
 
 				case "email":
 					var emailNotificationConfig c.EmailNotificationConfig
-					mapstructure.Decode(notificationReceiver, &emailNotificationConfig)
+					utils.Check(mapstructure.Decode(notificationReceiver, &emailNotificationConfig))
 					email, ok := notificationConfigs.Email.SmtpConfigsMap[emailNotificationConfig.From]
 					if !ok {
 						fmt.Println("Error: Bot not found in config.", emailNotificationConfig.From)
@@ -93,8 +103,12 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 						To:      emailNotificationConfig.To,
 						Subject: emailNotificationConfig.Subject,
 					})
-					notifier.NotifySpecificPortHealthCheckResult(healthCheckResult.Results[i], emailNotificationConfig.Template)
+					err = notifier.NotifySpecificPortHealthCheckResult(healthCheckResult.Results[i], emailNotificationConfig.Template)
+					if err != nil {
+						fmt.Println(err)
+					}
 					// fmt.Println("telegram notification", telegramNotificationConfig.)
+
 				default:
 					fmt.Println("unknown strategy")
 				}
@@ -123,12 +137,14 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 			if runConditionHasBeenAchieved {
 				fmt.Println("rule:", healthCheckResult.NumberOfUnreachableServices, "failed")
 				for _, notificationReceiver := range rule.Notify {
+
 					var notificationStrategyConfig c.NotificationStrategyConfig
-					mapstructure.Decode(notificationReceiver, &notificationStrategyConfig)
+					utils.Check(mapstructure.Decode(notificationReceiver, &notificationStrategyConfig))
+
 					switch notificationStrategyConfig.Via {
 					case "telegram":
 						var telegramNotificationConfig c.TelegramNotificationConfig
-						mapstructure.Decode(notificationReceiver, &telegramNotificationConfig)
+						utils.Check(mapstructure.Decode(notificationReceiver, &telegramNotificationConfig))
 						bot, ok := notificationConfigs.Telegram.TelegramBotsMap[telegramNotificationConfig.From]
 						if !ok {
 							fmt.Println("Error: Bot not found in config.", telegramNotificationConfig.From)
@@ -137,12 +153,16 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 						if !ok {
 							fmt.Println("Error: Bot not found in config.", telegramNotificationConfig.Chat)
 						}
+
 						notifier := n.NewTelegramNotifier(bot, chat)
-						notifier.NotifyHealthCheckResult(healthCheckResult, telegramNotificationConfig.Template)
+						err = notifier.NotifyHealthCheckResult(healthCheckResult, telegramNotificationConfig.Template)
+						if err != nil {
+							fmt.Println(err)
+						}
 						// fmt.Println("telegram notification", telegramNotificationConfig.)
 					case "slack":
 						var slackNotificationConfig c.SlackNotificationConfig
-						mapstructure.Decode(notificationReceiver, &slackNotificationConfig)
+						utils.Check(mapstructure.Decode(notificationReceiver, &slackNotificationConfig))
 						app, ok := notificationConfigs.Slack.SlackAppsMap[slackNotificationConfig.From]
 						if !ok {
 							fmt.Println("Error: Slack App not found in config.", slackNotificationConfig.From)
@@ -153,12 +173,15 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 						}
 
 						notifier := n.NewSlackNotifier(app, channel)
-						notifier.NotifyHealthCheckResult(healthCheckResult, slackNotificationConfig.Template)
+						err = notifier.NotifyHealthCheckResult(healthCheckResult, slackNotificationConfig.Template)
+						if err != nil {
+							fmt.Println(err)
+						}
 						// fmt.Println("telegram notification", telegramNotificationConfig.)
 
 					case "email":
 						var emailNotificationConfig c.EmailNotificationConfig
-						mapstructure.Decode(notificationReceiver, &emailNotificationConfig)
+						utils.Check(mapstructure.Decode(notificationReceiver, &emailNotificationConfig))
 						email, ok := notificationConfigs.Email.SmtpConfigsMap[emailNotificationConfig.From]
 						if !ok {
 							fmt.Println("Error: Bot not found in config.", emailNotificationConfig.From)
@@ -167,9 +190,13 @@ func RunHealthCheck(targetConfig c.TargetConfigurations, notificationConfigs c.N
 							To:      emailNotificationConfig.To,
 							Subject: emailNotificationConfig.Subject,
 						})
-						notifier.NotifyHealthCheckResult(healthCheckResult, emailNotificationConfig.Template)
+						err = notifier.NotifyHealthCheckResult(healthCheckResult, emailNotificationConfig.Template)
+						if err != nil {
+							fmt.Println(err)
+						}
 					default:
 						fmt.Println("unknown strategy")
+
 					}
 				}
 			}
@@ -193,49 +220,41 @@ func main() {
 			for _, v := range configuration.Notifications.Telegram.Bots {
 				botsMap[v.Name] = v
 			}
-
 			configuration.Notifications.Telegram.TelegramBotsMap = botsMap
-
 			chatsMap := make(map[string]c.TelegramChatConfiguration)
-
 			for _, v := range configuration.Notifications.Telegram.Chats {
 				chatsMap[v.Name] = v
 			}
-
 			configuration.Notifications.Telegram.TelegramChatsMap = chatsMap
-
 			smtpMap := make(map[string]c.SmtpConfiguration)
-
 			for _, v := range configuration.Notifications.Email.Smtp {
 				smtpMap[v.Name] = v
 			}
-
 			configuration.Notifications.Email.SmtpConfigsMap = smtpMap
-
 			slackAppsMap := make(map[string]c.SlackAppConfiguration)
-
 			for _, v := range configuration.Notifications.Slack.Apps {
 				slackAppsMap[v.Name] = v
 			}
-
 			configuration.Notifications.Slack.SlackAppsMap = slackAppsMap
-
 			slackChannelsMap := make(map[string]c.SlackChannelConfiguration)
-
 			for _, v := range configuration.Notifications.Slack.Channels {
 				slackChannelsMap[v.Name] = v
 			}
-
 			configuration.Notifications.Slack.SlackChannelsMap = slackChannelsMap
-
 			if len(configuration.Targets) == 0 {
 				fmt.Println("No targets specified. Nothing to do.")
 			}
 			for _, target := range configuration.Targets {
 				c := cron.New()
+
 				go RunHealthCheck(target, configuration.Notifications)()
-				c.AddFunc(target.Cron, RunHealthCheck(target, configuration.Notifications))
+
+				err := c.AddFunc(target.Cron, RunHealthCheck(target, configuration.Notifications))
+
+				utils.Check(err)
+
 				go c.Start()
+
 			}
 			sig := make(chan os.Signal, 1)
 			signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
@@ -243,6 +262,9 @@ func main() {
 			fmt.Print("Exiting...")
 		},
 	}
+
 	rootCmd.Flags().StringVar(&configFilePath, "config", "", "config file (default is path/config.yaml)")
-	rootCmd.Execute()
+
+	utils.Check(rootCmd.Execute())
+
 }
